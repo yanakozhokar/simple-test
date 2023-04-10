16f3bad2df0e8e4eb73ae5756c8f5fb97664afae
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-04-10 18:38:02 +0300

    updated

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'database' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'yana' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '1111' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
